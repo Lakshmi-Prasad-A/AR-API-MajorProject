01aeaf4fcd097f9fbc5e76b5461d5c2a73d11e87
veeru134005 <babu.veera010@gmail.com>
2023-01-30 14:49:54 +0530

    static variable added in user interface

--- src/main/java/com/hcsc/serviceImpl/UserRegInter.java
+++ src/main/java/com/hcsc/serviceImpl/UserRegInter.java
@@ -3,6 +3,8 @@ package com.hcsc.serviceImpl;
 import com.hcsc.entity.UserRegistration;
 
 public interface UserRegInter {
+	
+	public static String RHODE_ISLAND="Rhode Island";
 
 	public String userRegistration(UserRegistration userRegistration);
 }
